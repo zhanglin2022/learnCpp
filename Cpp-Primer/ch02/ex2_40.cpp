7c4007bec4d84cafcffe9318f37b1c44b19923dd
zhanglin2022 <1243265429@qq.com>
2024-08-16 11:07:25 +0800

    complete ex2_40

---
+++ Cpp-Primer/ch02/ex2_40.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+struct Sales_data {
+    std::string bookNo;
+    std::string bookName;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+    double price = 0.0;
+    // ...
+};
