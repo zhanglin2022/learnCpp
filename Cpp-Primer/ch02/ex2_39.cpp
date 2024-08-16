2aed35ace84d06860725feb92747ba763350fdeb
zhanglin2022 <1243265429@qq.com>
2024-08-16 10:54:08 +0800

    complete ex2_39

---
+++ Cpp-Primer/ch02/ex2_39.cpp
@@ -0,0 +1,10 @@
+/*
+* Compile the following program to see what happens when you forget the 
+* semicolon after a class definition. Remember the message for future reference
+*/
+
+// struct Foo { /* empty */} //Note: no semicolon
+
+int main() {
+    return 0;
+}
