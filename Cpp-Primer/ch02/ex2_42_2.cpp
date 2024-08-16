06cefa0108e28576ec918c7a7196dc38d72b7ea7
zhanglin2022 <1243265429@qq.com>
2024-08-16 21:56:01 +0800

    complete ex2_42

---
+++ Cpp-Primer/ch02/ex2_42_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "ex2_42.h"
+
+// 1.5.2 Add two Sales_data objects using the head file
+int main() {
+    Sales_data book1, book2;
+    double price1, price2;
+
+    std::cin >> book1.bookNo >> book1.units_sold >> price1;
+    std::cin >> book2.bookNo >> book2.units_sold >> price2;
+    book1.calRevenue(price1);
+    book2.calRevenue(price2);
+    
+    if (book1.bookNo == book2.bookNo) {
+        book1.addData(book2);
+        book1.printData();
+        return 0;
+    } else {
+        std::cout << "Data must be the same ISBN." << std::endl;
+        return -1;
+    }
+}
---
+++ Cpp-Primer/ch02/ex2_42_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "ex2_42.h"
+
+// 1.6 The Bookstore Program using the header file
+int main() {
+    Sales_data total;
+    double totalPrice = 0.0;
+    if (std::cin >> total.bookNo >> total.units_sold >> totalPrice) {
+        total.calRevenue(totalPrice);
+        Sales_data trans;
+        double transPrice = 0.0;
+        while (std::cin >> trans.bookNo >> trans.units_sold >> transPrice) {
+            trans.calRevenue(transPrice);
+            if (total.bookNo == trans.bookNo) {
+                total.addData(trans);
+            } else {
+                total.printData();
+                total.setData(trans);
+            }
+        }
+        total.printData();
+        return 0;
+    } else {
+        // no input! warn the user 
+        std::cerr << "No data?!" << std::endl; 
+        return -1; // indicate failure
+    }
+}
