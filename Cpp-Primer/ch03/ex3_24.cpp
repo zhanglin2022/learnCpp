98b4332756fd85793101ed858fbdbafbb210c081
zhanglin2022 <1243265429@qq.com>
2024-09-10 20:11:18 +0800

    complete ex3_24

---
+++ Cpp-Primer/ch03/ex3_24.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    vector<int> ivec;
+    for(int num = 0; cin >> num; ivec.push_back(num));
+
+    if (ivec.size() < 2) {
+        cout << "please enter at least 2 integers";
+        return -1;
+    }
+    for (auto it = ivec.cbegin(); it != ivec.cend() - 1; ++it)
+        cout << *it + *(it+1) << " ";
+    cout << endl;
+
+    for (auto lft = ivec.cbegin(), rht = ivec.cend() - 1; lft < rht; ++lft, --rht)
+        cout << *lft + *rht << " ";
+    cout << endl;
+
+    return 0;
+}
