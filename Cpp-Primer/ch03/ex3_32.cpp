f6cad5948214f44b2dcdb2c969d5f51af7c8b1a4
zhanglin2022 <1243265429@qq.com>
2024-10-04 18:41:24 +0800

    complete ex3_32

---
+++ Cpp-Primer/ch03/ex3_32.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main() {
+    constexpr size_t size = 10;
+    // array
+    int arr1[size];
+    for (size_t ix = 0; ix < size; ++ix) arr1[ix] = ix;
+    int arr2[size];
+    for (size_t ix = 0; ix < size; ++ix) arr2[ix] = arr1[ix];
+
+    // vector
+    vector<int> vec1(size);
+    for (int i = 0; i < size; ++i) vec1[i] = arr1[i];
+    vector<int> vec2(vec1);
+    for (auto i : vec2) cout << i << " ";
+    cout << endl;
+
+    return 0;
+}
