4c58b725c34b2512f100bf513f7e170f856275c4
zhanglin2022 <1243265429@qq.com>
2024-11-08 15:50:03 +0800

    modify ex4_22

--- Cpp-Primer/ch04/ex4_22.cpp
+++ Cpp-Primer/ch04/ex4_22.cpp
@@ -2,14 +2,23 @@
 #include<vector>
 
 using std::vector;
+using std::cin;
 using std::cout;
 using std::endl;
 
 int main () {
-    vector<int> grades = {99, 80, 72, 63, 55};
-    for (auto grade : grades)
-        cout << ((grade > 90) ? "high pass"
-                              : (grade > 75) ? "low pass"
-                              : (grade < 60) ? "fail" : "pass") << endl;
+
+    for (unsigned g; cin >> g;) {
+        // condition operators
+        cout << ((g > 90) ? "high pass"
+                          : (g < 60) ? "fail"
+                          : (g < 75) ? "low pass" : "pass") << endl;
+        // if statements
+        if (g > 90)      cout << "hign pass";
+        else if (g < 60) cout << "fail";
+        else if (g < 75) cout << "low pass";
+        else             cout << "pass";
+        cout << endl;
+    }    
     return 0;
 }
