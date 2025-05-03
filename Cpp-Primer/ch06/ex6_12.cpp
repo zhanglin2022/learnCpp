eab758838420cbf43bd902a172da072943a7bc08
zhanglin2022 <1243265429@qq.com>
2025-05-03 11:13:59 +0800

    complete ex6_12

---
+++ Cpp-Primer/ch06/ex6_12.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+void swap(int &a, int &b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    for(int i, j; cout << "Enter two numbers: \n", cin >> i >> j;) {
+        swap(i, j);
+        cout << i << " " << j << endl;
+    }
+    return 0;
+}
