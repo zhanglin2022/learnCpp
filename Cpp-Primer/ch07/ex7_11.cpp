06376fa1842d5e2a1bc51ae1a1aeed28073f8a08
zhanglin2022 <1243265429@qq.com>
2025-05-24 17:54:00 +0800

    modify ex7_11

--- Cpp-Primer/ch07/ex7_11.cpp
+++ Cpp-Primer/ch07/ex7_11.cpp
@@ -9,10 +9,10 @@ int main() {
     Sales_data item1;
     print(cout, item1) << endl;
 
-    Sales_data item2("1111");
+    Sales_data item2("0-201-78345-X");
     print(cout, item2) << endl;
 
-    Sales_data item3("2222", 3, 10.00);
+    Sales_data item3("0-201-78345-X", 3, 10.00);
     print(cout, item3) << endl;
 
     Sales_data item4(cin);
