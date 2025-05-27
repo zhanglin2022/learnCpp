50c1123afe2822874d32c721d863058d1708fe2d
zhanglin2022 <1243265429@qq.com>
2025-05-27 12:23:14 +0800

    complete ex7_27

---
+++ Cpp-Primer/ch07/ex7_27.cpp
@@ -0,0 +1,12 @@
+#include "ex7_27.h"
+#include <iostream>
+
+using std::cout;
+
+int main() {
+    Screen myScreen(5, 5, 'X'); 
+    myScreen.move(4,0).set('#').display(cout); 
+    cout << "\n"; 
+    myScreen.display(cout); 
+    cout << "\n";
+}
