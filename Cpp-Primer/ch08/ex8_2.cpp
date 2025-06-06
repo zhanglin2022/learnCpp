c3e72021df26afefaa2cc144f2b8067013f73ab7
zhanglin2022 <1243265429@qq.com>
2025-06-06 17:01:15 +0800

    complete ex8_2

---
+++ Cpp-Primer/ch08/ex8_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::istream;
+
+istream& func(istream &is) {
+    string buf;
+    while(is >> buf) 
+        std::cout << buf << std::endl;
+    
+    is.clear();
+    return is;
+}
+
+int main() {
+    istream& is = func(std::cin);
+    std::cout << is.rdstate() << std::endl;
+    
+    return 0;
+}
