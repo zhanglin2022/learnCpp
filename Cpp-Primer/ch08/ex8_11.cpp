37ed391a8b8c44adae26d152116ffc81be1cae02
zhanglin2022 <1243265429@qq.com>
2025-06-07 20:06:56 +0800

    complete ex8_11

---
+++ Cpp-Primer/ch08/ex8_11.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::istringstream;
+using std::string;
+using std::vector;
+using std::cin;
+
+struct PersonInfo{
+    string name;
+    vector<string> phones;
+};
+
+int main() {
+    string line, word;
+    vector<PersonInfo> people;
+    istringstream record;
+    while (getline(cin, line)) {
+        PersonInfo info;
+        record.clear();   // reset the status
+        record.str(line); // copy the string line into record
+        record >> info.name;
+        while (record >> word) 
+            info.phones.push_back(word);
+        people.push_back(info);
+    }
+
+    for (auto &p : people) {
+        std::cout << p.name << " ";
+        for (auto &s : p.phones)
+            std::cout << s << " ";
+        std::cout << std::endl;
+    }
+    
+    return 0;
+}
