9fe9528195192804467123c3ddaf0a46db206849
zhanglin2022 <1243265429@qq.com>
2025-06-10 11:44:10 +0800

    modify ex9_24

--- Cpp-Primer/ch09/ex9_24.cpp
+++ Cpp-Primer/ch09/ex9_24.cpp
@@ -8,6 +8,6 @@ int main() {
     cout << vec.at(0);      // terminate called after throwing an instance of 'std::out_of_range'
     cout << vec[0];         // Segmentation fault
     cout << vec.front();    // Segmentation fault
-    cout << *vec.begin();      // Segmentation fault
+    cout << *vec.begin();   // Segmentation fault
     return 0;
 }
