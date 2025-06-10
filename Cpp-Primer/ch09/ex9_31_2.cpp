753d49a0f03e4f9f473b0f5499ebd34b044b5280
zhanglin2022 <1243265429@qq.com>
2025-06-10 17:10:39 +0800

    complete ex9_31

---
+++ Cpp-Primer/ch09/ex9_31_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <forward_list>
+
+using std::forward_list; using std::cout; using std::endl; using std::advance;
+
+auto remove_evens_and_double_odds(forward_list<int>& data) {
+    auto pre = data.before_begin();
+    for (auto cur = data.begin(); cur != data.end();) {
+        if (*cur & 0x1) {
+            cur = data.insert_after(pre, *cur);
+            advance(pre, 2);
+            advance(cur, 2);
+        } else {
+            cur = data.erase_after(pre);
+        }
+        
+    }
+}
+
+int main() {
+    forward_list<int> fli = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    remove_evens_and_double_odds(fli);
+    for (auto i : fli) cout << i << " ";
+    cout << endl;
+}
