46b90300b8a1a49ba9b42a4bc3b49542ed377519
zhanglin2022 <1243265429@qq.com>
2025-06-10 10:05:07 +0800

    modify ex9_18

--- Cpp-Primer/ch09/ex9_18.cpp
+++ Cpp-Primer/ch09/ex9_18.cpp
@@ -5,9 +5,9 @@
 using std:: cin; using std::cout; using std::endl; using std::string; using std::deque;
 
 int main() {
-    deque<string> dqe;
-    for(string str; cin >> str; dqe.push_back(str));
-    for (auto it = dqe.cbegin(); it != dqe.cend(); ++it)
+    deque<string> input;
+    for(string str; cin >> str; input.push_back(str));
+    for (auto it = input.cbegin(); it != input.cend(); ++it)
         cout << *it << endl;
 
     return 0;
