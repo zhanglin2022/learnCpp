7c15795a1a388d4131d3212ad9f4d5f62ba142f8
zhanglin2022 <1243265429@qq.com>
2025-06-12 11:35:12 +0800

    complete ex9_47

---
+++ Cpp-Primer/ch09/ex9_47_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using std::string; using std::cout; using std::endl;
+
+int main() {
+    string s("ab2c3d7R4E6");
+    string nums("123456789");
+    string apbts("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    // use find_the_first_not_of
+    cout << "numeric characters: ";
+    for (size_t pos = 0; (pos = s.find_first_not_of(apbts, pos)) != string::npos; ++pos) 
+        cout << s[pos] << " ";
+    cout << "\nalphabetic characters: ";
+    for (size_t pos = 0; (pos = s.find_first_not_of(nums, pos)) != string::npos; ++pos)
+        cout << s[pos] << " ";
+    cout << endl;
+
+    return 0;
+}
