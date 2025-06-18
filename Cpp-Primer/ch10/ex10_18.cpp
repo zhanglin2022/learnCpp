b7f1c7c72774bc51a51ce60a6a48858dc41bf146
zhanglin2022 <1243265429@qq.com>
2025-06-18 19:56:25 +0800

    complete ex10_18

---
+++ Cpp-Primer/ch10/ex10_18.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::vector; using std::string; using std::cout; using std::endl;
+
+void elimDups(vector<string> &words) {
+    std::sort(words.begin(), words.end());
+    auto end_unique = unique(words.begin(), words.end());
+    words.erase(end_unique, words.end());
+}
+
+void biggies_partition(vector<string> &words, vector<string>::size_type sz) {
+    elimDups(words); 
+    auto pivot = std::partition(words.begin(), words.end(), [sz] (const string &str) 
+                                { return str.size() >= sz;} );
+    for (auto it = words.cbegin(); it != pivot; ++it) cout << *it << " ";
+    cout << endl;
+}
+
+int main() {
+    vector<string> svec{"the", "quick", "red", "fox", "jumps", "over", "the", "slow", 
+                        "red", "turtle"};
+    biggies_partition(svec, 3);
+
+    return 0;
+}
