00c6594ff9bc91af76a4c61d1025f72511e13587
zhanglin2022 <1243265429@qq.com>
2025-06-18 19:38:51 +0800

    complete ex10_17

---
+++ Cpp-Primer/ch10/ex10_17.cpp
@@ -0,0 +1,20 @@
+#include "../ch07/ex7_26.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+using std::vector; using std::ifstream; using std::cout; using std::endl;
+
+int main() {
+    Sales_data d1("aa"), d2("aaaa"), d3("aaa"), d4("y"), d5("aaaaz");
+    vector<Sales_data> svec{d1, d2, d3, d4, d5};
+    
+    std::sort(svec.begin(), svec.end(), [](const Sales_data &s1, const Sales_data &s2) 
+         { return s1.isbn().size() < s2.isbn().size();} );
+
+    for (const auto &s : svec) cout << s.isbn() << " ";
+    cout << endl; 
+    
+    return 0;
+}
