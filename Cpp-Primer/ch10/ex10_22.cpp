75b24b62ab678ad5ac4d974e92c9269cd8d4a99c
zhanglin2022 <1243265429@qq.com>
2025-06-28 19:46:49 +0800

    complete ex10_22

---
+++ Cpp-Primer/ch10/ex10_22.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <functional>
+
+using std::vector; using std::string; using std::cout; using std::endl;
+using std::placeholders::_1;
+
+bool check_size(const string &s, string::size_type sz) {
+    return s.size() <= sz;
+}
+
+int main() {
+    vector<string> svec{"obliged", "puma", "investigate", "corner"};
+    auto i = std::count_if(svec.begin(), svec.end(), std::bind(check_size, _1, 6));
+    cout << i << endl;
+
+    return 0;
+}
