f5c74bccdc2df96cf8c998b4065d63e8d1f444bc
zhanglin2022 <1243265429@qq.com>
2025-07-03 16:28:40 +0800

    complete ex10_37

---
+++ Cpp-Primer/ch10/ex10_37.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main() {
+    std::vector<int> vec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::list<int> rlst(8 - 3);
+    std::copy(vec.cbegin() + 3, vec.cbegin() + 8, rlst.rbegin());   
+    //     0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    //           ^              ^
+    //          rend          rbegin
+    // @note: std::copy copies the range [first, last) into result.
+    //        hence, the arguments here denote:
+    //        [7 6 5 4 3 2)
+    //                   ^ this one is specified but not included. 
+    for (auto i : rlst) std:: cout << i << " ";
+    std::cout << std::endl;
+    
+    return 0;
+}
