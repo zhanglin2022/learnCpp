7a5bc320be63c28140207a1c947942ad7c7eea67
zhanglin2022 <1243265429@qq.com>
2025-07-05 15:20:24 +0800

    modify ex11_3

--- Cpp-Primer/ch11/ex11_3.cpp
+++ Cpp-Primer/ch11/ex11_3.cpp
@@ -7,12 +7,11 @@ int main() {
     std::map<std::string, size_t> word_count;
     std::set<std::string> exclude = { "The", "But", "And", "Or", "An", "A", 
                                       "the", "but", "and", "or", "an", "a" };
-    
     for (std::string word; std::cin >> word; ) {
         if (exclude.find(word) == exclude.end()) ++word_count[word];
     }
-
     for (const auto &w : word_count)
         std::cout << w.first << " : " << w.second << std::endl;
+    
     return 0;
 }
