883ef09bca7a3555429c5262c1d8f32f098e0ebf
zhanglin2022 <1243265429@qq.com>
2025-07-09 21:00:19 +0800

    complete ex11_20

--- Cpp-Primer/ch11/ex11_20.cpp
+++ Cpp-Primer/ch11/ex11_20.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+#include <map>
+#include <string>
+
+using std::cin; using std::cout; using std::endl; using std::map; using std::string;
+
+int main() {
+    map<string, size_t> word_count; 
+    string word; 
+    for (string word; cin >> word; ) {
+        auto ret = word_count.insert({word, 1});
+        if (!ret.second) 
+        ++ret.first->second;
+    }        
+    for (const auto &w : word_count) 
+        cout << w.first << ": " << w.second << endl;
+
+    return 0;
+}
