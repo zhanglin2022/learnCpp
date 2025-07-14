279f953047409b60c11b35abb759abc99dbb06bc
zhanglin2022 <1243265429@qq.com>
2025-07-14 19:12:28 +0800

    modify files

--- Cpp-Primer/ch11/ex11_33.cpp
+++ Cpp-Primer/ch11/ex11_33.cpp
@@ -8,41 +8,47 @@
 using std::map; using std::string; using std::cout; using std::endl; 
 using std::ifstream; using std::runtime_error; using std::istringstream;
 
+void print (map<string, string> &ma) {
+    for (auto kv : ma)
+        cout << kv.first << ": " << kv.second << endl;
+        cout << endl;
+}
+
 map<string, string> buildMap (ifstream &map_file) {
     map<string, string> trans_map;
-    string key, value;
-    while (map_file >> key && getline(map_file, value)) {
+    for (string key, value; map_file >> key && getline(map_file, value); ) {
         if (value.size() > 1 ) trans_map[key] = value.substr(1);
         else throw runtime_error("no rule for "+key);
     }
     return trans_map;
 }
 
-string const& transform(const string &s, const map<string, string> &m) {
+const string & transform(const string &s, const map<string, string> &m) {
     auto it = m.find(s);
-    return it == m.cend() ? it->second : s;
+    return it == m.cend() ? s : it->second;
 }
 
 void word_transform(ifstream &map_file, ifstream &input) {
     auto trans_map = buildMap(map_file);
-    string text;
-    while (getline(input, text)) {
-        istringstream stream(text);
-        string word;
+    for (string text; getline(input, text); ) {
+        istringstream iss(text);
         bool firstword = true;
-        while (stream >> word) {
+        for (string word; iss >> word; ) {
             if (firstword) firstword = false;
             else cout << " ";
-            cout << transform(word, trans_map);
+            std::cout << transform(word, trans_map);
         }
+            
         cout << endl;
     }
 }
 
-int main (int argc, char** argv) {
-    if (argc != 3) throw runtime_error("file number not match");
-    ifstream map_file(argv[1]), input_file(argv[2]);
-    word_transform(map_file, input_file);
-
+int main () {
+    ifstream map_file("../../data/rule_of_transform.txt");
+    ifstream input_file("../../data/give_to_transform.txt");
+    if (map_file && input_file)
+        word_transform(map_file, input_file);
+    else 
+        std::cerr << "can not file documents\n";
     return 0;
 }
