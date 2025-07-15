89f07c0f8fd5e54a639b67396589efd67ed18fae
zhanglin2022 <1243265429@qq.com>
2025-07-15 17:21:57 +0800

    complete ex11_38

---
+++ Cpp-Primer/ch11/ex11_38.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::unordered_map; using std::string; using std::cin; using std::cout;
+using std::endl; using std::ifstream; using std::istringstream;
+
+void wordCounting() {
+    unordered_map<string, size_t> counts;
+    for (string word; cin >> word; ++counts[word]);
+    for (const auto &c : counts) 
+        cout << c.first << ": " << c.second << endl;
+}
+
+void wordTransformation() {
+    ifstream ifs_map("../../data/rule_of_transform.txt");
+    ifstream ifs_input("../../data/give_to_transform.txt");
+    if (!ifs_map || !ifs_input) {
+        std::cout << "can't find file documents\n";
+        return;
+    } 
+    unordered_map<string, string> trans_map;
+    for (string key, value; ifs_map >> key && getline(ifs_map, value); ) {
+        if (value.size() > 1 ) trans_map[key] = value.substr(1).substr(0, value.find_last_not_of(" "));
+    }
+
+    for (string text; getline(ifs_input, text); ) {
+        istringstream iss(text);
+        bool firstword = true;
+        for (string word; iss >> word; ) {
+            if (firstword) firstword = false;
+            else cout << " ";
+            auto map_it = trans_map.find(word);
+            cout << (map_it == trans_map.cend() ? word : map_it->second);
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    // wordCounting();
+    wordTransformation();
+
+    return 0;
+}
