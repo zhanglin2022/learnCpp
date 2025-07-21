68ad546599b52a2bc07bd4f45112d2ee03ecb251
zhanglin2022 <1243265429@qq.com>
2025-07-21 18:28:56 +0800

    modify ex12_11

--- Cpp-Primer/ch12/ex12_11.cpp
+++ Cpp-Primer/ch12/ex12_11.cpp
@@ -11,9 +11,9 @@ int main() {
     shared_ptr<int> p(new int(42));
     /**
      * @brief   std::shared_ptr<int>(p.get()) construct a temporary shared_ptr and copy it
-     *          to the parameter.However it is not a copy of p. As a result, at end of this
+     *          to the parameter. However it is not a copy of p. As a result, at end of this
      *          main function p will free the memory that has been freed inside process ().
-     *          That's why "double freed or corruption" was generated.
+     *          That's why "double free" was generated.
      */
     process(shared_ptr<int>(p.get()));
 
