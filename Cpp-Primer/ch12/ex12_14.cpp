77742a434f4c392752a4758d136e72d43afce837
zhanglin2022 <1243265429@qq.com>
2025-07-22 10:57:27 +0800

    complete ex12_14

---
+++ Cpp-Primer/ch12/ex12_14.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+struct connection {
+    std::string ip;
+    int port;
+    connection(std::string ip_, int port_): ip(ip_), port(port_) {}
+};
+struct destination {
+    std::string ip;
+    int port;
+    destination(std::string ip_, int port_):ip(ip_), port(port_) {}
+};
+
+connection connect(destination* pDest) {
+    std::shared_ptr<connection> pConn(new connection(pDest->ip, pDest->port));
+    std::cout << "creating connection(" << pConn.use_count() << ")" << std::endl;
+    return *pConn;
+}
+
+void disconnect(connection pConn) {
+    std::cout << "connection close(" << pConn.ip << ":" << pConn.port << ")" << std::endl;
+}
+
+void end_connection(connection *pConn) { 
+    disconnect(*pConn); 
+};
+
+void f(destination &d) {
+    connection conn = connect(&d);
+    std::shared_ptr<connection> p(&conn, end_connection);
+    std::cout << "connecting now(" << p.use_count() << ")" << std::endl;
+}
+
+int main() {
+    destination dest("192.168.133.83", 1243);
+    f(dest);
+}
