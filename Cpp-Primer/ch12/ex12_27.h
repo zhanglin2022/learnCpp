43a8a50611cc146546d45b07324d55451e94d5b0
zhanglin2022 <1243265429@qq.com>
2025-08-02 18:18:09 +0800

    add a default constructor

--- Cpp-Primer/ch12/ex12_27.h
+++ Cpp-Primer/ch12/ex12_27.h
@@ -3,8 +3,9 @@
 
 #include <iostream>
 #include <fstream>
-#include <strstream>
+#include <sstream>
 #include <memory>
+#include <algorithm>
 #include <vector>
 #include <map>
 #include <set>
@@ -17,16 +18,33 @@ class QueryResult;
 
 class TextQuery { 
 public:
-    using line_no = vector<string>::size_type;
+    using Line_no = vector<string>::size_type;
     TextQuery(ifstream&);
     QueryResult query(const string&) const;
 private:
-    // TODO: which data should be share  and how to share?
     // Constructor
     // function query()
-    shared_ptr<vector<string>> file; 
-    map<string, shared_ptr<set<line_no>>> word_line;
+    shared_ptr<vector<string>> input; 
+    map<string, shared_ptr<set<Line_no>>> word_line;
 };
+
+TextQuery::TextQuery(ifstream &ifs): input(new vector<string>) {
+    if (!ifs) std::cerr << "No input file" << endl;
+    Line_no line_no = 0;
+    for (string line; getline(ifs, line); ++line_no) {
+        input->push_back(line);
+        istringstream line_stream(line);
+        for (string text, word; line_stream >> text; word.clear()) {
+            std::remove_copy_if(text.begin(), text.end(), std::back_inserter(word), ispunct);
+            // use reference avoid count of shared_ptr add
+            // if word isn’t already in wm, subscripting adds a new entry
+            auto &lines = word_line[word];
+            // that pointer is null the first time we see word
+            if (!lines) lines.reset(new set<Line_no>); 
+            lines->insert(line_no);
+        }
+    }
+}
 
 
 
