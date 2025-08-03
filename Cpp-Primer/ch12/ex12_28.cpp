9ebb900560f06fb22da1a8d5a81ed7a27dd96dad
zhanglin2022 <1243265429@qq.com>
2025-08-03 20:31:20 +0800

    complete ex12_28

---
+++ Cpp-Primer/ch12/ex12_28.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+
+using std::cin; using std::cout; using std::endl; using std::vector; 
+using std::map; using std::set; using std::string; using std::shared_ptr; 
+using std::istringstream; using std::ifstream;
+
+using Line_no = vector<string>::size_type;
+
+int main() {
+    ifstream file("../../data/give_to_transform.txt");
+    vector<string> input;
+    map<string, set<Line_no>> word_line;
+    Line_no line_no = 0;
+    for (string line; getline(file, line); ++line_no) {
+        input.push_back(line);
+        istringstream line_stream(line);
+        for (string text, word; line_stream>>text; word.clear()) {
+            std::remove_copy_if(text.begin(), text.end(), std::back_inserter(word), ispunct);
+            word_line[word].insert(line_no);
+        }
+    }
+
+    while (true) {
+        std::cout << "enter word to look for, or q to quit: ";
+        string str;
+        if (!(std::cin >> str) || str == "q") break;
+        auto found = word_line.find(str);
+        if (found != word_line.end()) {
+            cout << str << " occurs " << found->second.size() << (found->second.size() > 1 ? " times" : " time") << endl;
+            for (auto i : found->second)
+            cout << "\t(line " << i+1 << ") " << input.at(i) << endl;
+        } else cout << str << "occurs 0 time" << endl;
+    }
+    return 0;
+}
