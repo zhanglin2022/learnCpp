40419e252d36408a9126a99cf49f52dff54c5adf
zhanglin2022 <1243265429@qq.com>
2025-08-04 19:42:50 +0800

    add ex12_33 test file

---
+++ Cpp-Primer/ch12/ex12_33_TEST.cpp
@@ -0,0 +1,20 @@
+#include "ex12_33.h"
+#include <iostream>
+
+void runQueries(std::ifstream &infile) {
+    TextQuery tq(infile);
+    do {
+        std::cout << "enter word to look for, or q to quit: ";
+        string s;
+        if (!(std::cin >> s) || s == "q") break;
+        print(std::cout, tq.query(s)) << std::endl;
+    } while (true);
+}
+
+int main()
+{
+    std::ifstream file("../../data/give_to_transform.txt");
+    runQueries(file);
+
+    return 0;
+}
