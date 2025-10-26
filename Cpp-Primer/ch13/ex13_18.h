113e06b6c6f8b512d7c2ed36e22e79ea6d42fe28
zhanglin2022 <1243265429@qq.com>
2025-10-26 21:39:53 +0800

    complete ex13_18

---
+++ Cpp-Primer/ch13/ex13_18.h
@@ -0,0 +1,21 @@
+#ifndef EX13_8_H
+#define EX13_8_H
+
+#include <string>
+
+using std::string;
+
+class Employee {
+public:
+    Employee() { id_ = s_increment++; }
+    Employee(string &name) { name_ = name; id_ = s_increment++; }
+
+    const int id() const {return id_; }
+
+private:
+    string name_;
+    int id_;
+    static int s_increment;
+};
+
+#endif
