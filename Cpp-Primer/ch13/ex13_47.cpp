1a39f14c367bed647459c0ec820d100a43c6cd96
zhanglin2022 <1243265429@qq.com>
2025-11-30 16:31:00 +0800

    complete ex13_47

---
+++ Cpp-Primer/ch13/ex13_47.cpp
@@ -0,0 +1,54 @@
+// Exercise 13.47
+// 
+// Give the copy constructor and copy-assignment operator in your String 
+// class from exercise 13.44 in § 13.5 (p. 531) a statement that prints a 
+// message each time the function is executed 
+//
+
+#include "ex13_47.h"
+#include <iostream>
+#include <algorithm>
+
+std::pair<char*, char*> 
+String::alloc_n_copy(const char* b, const char *e) {
+    auto data = alloc.allocate(e - b);
+    return { data, std::uninitialized_copy(b, e, data) };
+}
+
+void String::range_initialize(const char *first, const char *last) {
+    auto newdata = alloc_n_copy(first, last);
+    elements = newdata.first;
+    end = newdata.second;
+}
+
+String::String(const char *c) {
+    char *str = const_cast<char*>(c);
+    while (str)
+        ++str;
+    range_initialize(c, ++str);
+}
+
+String::String(const String &rhs) {
+    range_initialize(rhs.elements, rhs.end);
+    std::cout << "copy constructor" << std::endl;
+}
+
+void String::free() {
+    if (elements) {
+        std::for_each(elements, end, [this] (char &c) { alloc.destroy(&c); });
+        alloc.deallocate(elements, end - elements);
+    }
+}
+
+String& String::operator=(const String &rhs) {
+    auto newstr = alloc_n_copy(rhs.elements, rhs.end);
+    free();
+    elements = newstr.first;
+    end = newstr.second;
+    std::cout << "copy-assignment" << std::endl;
+    return *this;
+}
+
+int main() {
+    return 0;
+}
---
+++ Cpp-Primer/ch13/ex13_47.h
@@ -0,0 +1,37 @@
+// Exercise 13.47
+// 
+// Give the copy constructor and copy-assignment operator in your String 
+// class from exercise 13.44 in § 13.5 (p. 531) a statement that prints a 
+// message each time the function is executed 
+//
+
+#ifndef EX13_47_H
+#define EX13_47_H
+
+#include <memory>
+
+class String {
+public:
+    String(): String("") { } // Delegating Constructor
+    String(const char*);
+    String(const String&);
+    String& operator=(const String&);
+
+    const char* c_str() const { return elements; }
+    size_t size() const { return end - elements; }
+    size_t length() const { return end - elements + 1; }
+
+private:
+    std::pair<char*, char*> alloc_n_copy(const char*, const char*);
+    void range_initialize(const char*, const char*);
+    void free();
+
+private:
+    char *elements;
+    char *end;
+    static std::allocator<char> alloc;
+};
+
+std::allocator<char> String::alloc;
+
+#endif
