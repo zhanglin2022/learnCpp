a266d21150155ec9af4fbb7e6e299774aa304184
zhanglin2022 <1243265429@qq.com>
2026-01-27 17:02:36 +0800

    complete ex14_2

---
+++ Cpp-Primer/ch14/ex14_02_TEST.cpp
@@ -0,0 +1,15 @@
+// Exercise 14.2
+// 
+// Write declarations for the overloaded input, output, addition, and
+// compound-assignment operators for Sales_data.
+//
+#include "ex14_02.h"
+
+int main() {
+    
+    Sales_data s1;   
+    std::cin >> s1;
+    std::cout << s1 << std::endl;
+
+    return 0;
+}
