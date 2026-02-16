0f33baef897b8206fd36bb067857dd2a8187b32c
zhanglin2022 <1243265429@qq.com>
2026-02-16 18:28:16 +0800

    complete ex14_16 class StrBlob and const version

---
+++ Cpp-Primer/ch14/ex14_16_StrBlob.h
@@ -0,0 +1,114 @@
+// Exercise 14.16
+//
+// @See Exercise 12.19, Exercise 12.22
+//
+// Define equality and inequality operators for your StrBlob (§ 12.1.1, p. 456), 
+// StrBlobPtr (§ 12.1.6, p. 474), StrVec (§ 13.5, p. 526), and String (§ 13.5, p. 531)
+// classes.
+//
+
+#ifndef EX14_16_STRBLOB_H
+#define EX14_16_STRBLOB_H
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <initializer_list>
+#include <stdexcept>
+
+class StrBlobPtr;
+class ConstStrBlobPtr;
+
+// class StrBlob
+
+class StrBlob {
+    friend class StrBlobPtr;
+    friend class ConstStrBlobPtr;
+    friend bool operator==(const StrBlob&, const StrBlob&);
+    friend bool operator!=(const StrBlob&, const StrBlob&);
+
+public:
+    using size_type = std::vector<std::string>::size_type;
+
+    StrBlob(): data(std::make_shared<std::vector<std::string>>()) {}
+    StrBlob(std::initializer_list<std::string> il): data(std::make_shared<std::vector<std::string>>(il)) {}
+
+    StrBlob(const StrBlob &sb) : data(std::make_shared<std::vector<std::string>>(*sb.data)) { }
+    StrBlob& operator=(const StrBlob&);
+
+    StrBlob(const StrBlob &&rhs)  noexcept : data(std::move(rhs.data)) { }
+    StrBlob& operator=(StrBlob&&) noexcept;
+
+    StrBlobPtr begin();
+    StrBlobPtr end();
+    ConstStrBlobPtr cbegin() const;
+    ConstStrBlobPtr cend() const;
+
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+
+    void push_back(const std::string &t) { data->push_back(t); }
+    void pop_back();
+
+    std::string& front();
+    std::string& back();
+    const std::string& front() const;
+    const std::string& back() const;
+
+private:
+    void check(size_type i, const std::string &msg) const;
+
+private:
+    std::shared_ptr<std::vector<std::string>> data;
+};
+
+bool operator==(const StrBlob&, const StrBlob&);
+bool operator!=(const StrBlob&, const StrBlob&);
+
+// class StrBlobPtr
+
+class StrBlobPtr {
+    friend bool operator==(const StrBlobPtr&, const StrBlobPtr&);
+    friend bool operator!=(const StrBlobPtr&, const StrBlobPtr&);
+public:
+    StrBlobPtr(): curr(0) { }
+    StrBlobPtr(StrBlob &a, size_t sz = 0):
+            wptr(a.data), curr(sz) { }
+    bool operator!=(const StrBlobPtr &p) { return p.curr != curr; }
+    std::string& deref() const;
+    StrBlobPtr& incr();
+
+private:
+    std::shared_ptr<std::vector<std::string>> 
+        check(std::size_t, const std::string&) const;
+    std::weak_ptr<std::vector<std::string>> wptr;
+    std::size_t curr;
+};
+
+bool operator==(const StrBlobPtr&, const StrBlobPtr&);
+bool operator!=(const StrBlobPtr&, const StrBlobPtr&);
+
+// class ConstStrBlobPtr
+
+class ConstStrBlobPtr {
+    friend bool operator==(const ConstStrBlobPtr&, const ConstStrBlobPtr&);
+    friend bool operator!=(const ConstStrBlobPtr&, const ConstStrBlobPtr&);
+public:
+    ConstStrBlobPtr(): curr(0) { }
+    ConstStrBlobPtr(const StrBlob &a, size_t sz = 0):
+            wptr(a.data), curr(sz) { } // should add const
+    bool operator!=(const ConstStrBlobPtr &p) { return p.curr != curr; }
+    const std::string& deref() const; // return value should add const
+    ConstStrBlobPtr& incr();
+
+private:
+    std::shared_ptr<std::vector<std::string>> 
+        check(std::size_t, const std::string&) const;
+    std::weak_ptr<std::vector<std::string>> wptr;
+    std::size_t curr;
+};
+
+bool operator==(const ConstStrBlobPtr&, const ConstStrBlobPtr&);
+bool operator!=(const ConstStrBlobPtr&, const ConstStrBlobPtr&);
+
+#endif
