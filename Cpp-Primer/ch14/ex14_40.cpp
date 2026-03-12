ee501d6f8cadb552d946394e50ebb2bb1eca7354
zhanglin2022 <1243265429@qq.com>
2026-03-12 10:32:32 +0800

    complete ex14_40

--- Cpp-Primer/ch14/ex14_40.cpp
+++ Cpp-Primer/ch14/ex14_40.cpp
@@ -11,35 +11,44 @@
 
 using std::string; using std::vector; using std::cout; using std::endl; 
 
-//return the plural version of word if ctr is greater than 1
+struct IsShorter {
+    bool operator()(const string &s1, const string &s2) const { return s1.size() < s2.size(); }
+};
+
+struct IsLongger {
+    IsLongger(const size_t sz): sz_(sz) { }
+    bool operator()(const string &str) const { return str.size() >= sz_; }
+private:
+    size_t sz_;
+};
+
+struct Print {
+    void operator()(const string &str) const { cout << str << " "; }
+};
+
 string make_plural(size_t ctr, const string &word, const string &ending) {
     return (ctr > 1) ? word + ending : word;
 }
 
 void elimDups(vector<string> &words) {
-    //sort words alphabetically so we can find the duplicates
     sort(words.begin(), words.end());
-    //unique reorders the input range so that each word appears once in the
-    //front portion of the range and returns an iterator one past the unique range
     auto end_unique = unique(words.begin(), words.end());
-    //erase uses a vector operation to remove the nonunique elements
     words.erase(end_unique, words.end());
 }
 
 void biggies(vector<string> &words, vector<string>::size_type sz) {
-    elimDups(words); //put words in alphabetical order and remove duplicates
-    //sort words by size, but maintain alphabetical order for words of the same size
-    stable_sort(words.begin(), words.end(), [](const string &a, const string &b) { return a.size() < b.size();});
-    //get an iterator to the first element whose size() is >= sz
-    auto wc = find_if(words.begin(), words.end(), [sz](const string &a) { return a.size() >= sz; });
-    //compute the number of elements with size >= sz
+    elimDups(words); 
+    stable_sort(words.begin(), words.end(), IsShorter());
+    auto wc = find_if(words.begin(), words.end(), IsLongger(sz));
     auto count = words.end() - wc;
-    cout << count << " " << make_plural(count, "word", "s")
-         << " of length " << sz << " or longer" << endl;
-    //print words of the given size or longer, each one followed by a space
-    for_each(wc, words.end(), [](const string &s){cout << s << " ";});
+    cout << count << " " << make_plural(count, "word", "s") << " of length " << sz << " or longer" << endl;
+    for_each(wc, words.end(), Print());
     cout << endl;
 }
 
-// TODO
+int main() {
+    vector<string> svec{"fox", "jumps", "over", "quick", "red", "red", "slow", "the", "turtle"};
+    biggies(svec, 4);
 
+    return 0;
+}
