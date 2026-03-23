4f89224e71788d535f8cddf0cde580c87dd4462e
zhanglin2022 <1243265429@qq.com>
2026-03-23 22:18:11 +0800

    move to folder

---
+++ Cpp-Primer/ch15/ex15.3/Quote.cpp
@@ -0,0 +1,13 @@
+// Exercise 15.3
+//
+// Define your own versions of the Quote class and the print_total function.
+// 
+
+#include "../ex15.3/Quote.h"
+
+void print_total(std::ostream &os, const Quote &item, size_t n) {
+    double ret = item.net_price(n);
+    std::cout << "ISBN: " << item.isbn()
+              << " # sold: " << n
+              << " total due: " << ret << std::endl;
+}
