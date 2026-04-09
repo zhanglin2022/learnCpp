f8d8183bfe2b716ba89a2ddea14df262988b35a8
zhanglin2022 <1243265429@qq.com>
2026-04-09 18:08:25 +0800

    add ex15_17

--- Cpp-Primer/ch15/ex15.15.16.17/main.cpp
+++ Cpp-Primer/ch15/ex15.15.16.17/main.cpp
@@ -1,31 +1,20 @@
 #include "quote.h"
 #include "bulk_quote.h"
 #include "limit_quote.h"
-
-void print_debug(const Quote &q) {
-    q.debug();
-}
+#include "disc_quote.h"
 
 int main() {
-    Quote q("12", 24.5);
-    Bulk_quote bq("34", 24.5, 5, 0.2);
-    Limit_quote lq("56", 24.5, 5, 0.2);
+    /*
+    error: cannot declare variable ‘d’ to be of abstract type ‘Disc_quote’:
+        Disc_quote d;
 
-    // not dynamic binding
-    q.debug();  
-    std::cout << std::endl;
-    bq.debug(); 
-    std::cout << std::endl;
-    lq.debug(); 
-    std::cout << "\n\n";
+    note: because the following virtual functions are pure within ‘Disc_quote’:
+        class Disc_quote: public Quote
 
-    // dynamic binding
-    print_debug(q);
-    std::cout << std::endl;
-    print_debug(bq);
-    std::cout << std::endl;
-    print_debug(lq);
-    std::cout << std::endl;
+    note: ‘virtual double Disc_quote::net_price(std::size_t) const’:
+        virtual double net_price(std::size_t) const override = 0;
+    */
+    Disc_quote d;
 
     return 0;
 }
