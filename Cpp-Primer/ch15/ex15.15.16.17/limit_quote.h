5314e8869d520e11b9c64ce970a25c419bcb3297
zhanglin2022 <1243265429@qq.com>
2026-04-09 17:37:34 +0800

    modify ex15_16

--- Cpp-Primer/ch15/ex15.15.16.17/limit_quote.h
+++ Cpp-Primer/ch15/ex15.15.16.17/limit_quote.h
@@ -1,20 +1,16 @@
 #ifndef LIMIT_QUOTE_H
 #define LIMIT_QUOTE_H
 
-#include "quote.h"
+#include "disc_quote.h"
 
-class Limit_quote: public Quote {
+class Limit_quote: public Disc_quote {
 public:
     Limit_quote() = default;
     Limit_quote(const std::string &b, double p, std::size_t qty, double disc):
-        Quote(b, p), max_qty(qty), discount(disc) { }
+        Disc_quote(b, p, qty, disc) { }
 
     double  net_price(std::size_t) const override;
     void    debug() const override;
-
-private:
-    std::size_t  max_qty   = 0;
-    double       discount  = 0.0;
 };
 
 #endif
