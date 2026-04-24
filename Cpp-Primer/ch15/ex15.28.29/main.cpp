bfff7b4d2250a2f941397333c6896be961d6d18a
zhanglin2022 <1243265429@qq.com>
2026-04-24 12:10:32 +0800

    modify ex15_29

--- Cpp-Primer/ch15/ex15.28.29/main.cpp
+++ Cpp-Primer/ch15/ex15.28.29/main.cpp
@@ -3,6 +3,7 @@
 #include "limit_quote.h"
 #include "disc_quote.h"
 #include <vector>
+#include <memory>
 
 int main() {
     /**
@@ -16,9 +17,21 @@ int main() {
     
     for (auto &v : vec)
         total += v.net_price(20);
-    
     std::cout << total << std::endl;
     std::cout << "======================\n\n";
+
+    /**
+     * @brief ex15.29    outcome == 6363
+     */
+
+    std::vector<std::shared_ptr<Quote>> pvec;
+    double total_p = 0.0;
+    for (unsigned i = 1; i != 10; ++i)
+        pvec.push_back(std::make_shared<Bulk_quote>(Bulk_quote("cc", i * 10.1, 10, 0.3)));
+
+    for (auto &p : pvec)
+        total_p += p->net_price(20);
+    std::cout << total_p << std::endl;
 
     return 0;
 }
