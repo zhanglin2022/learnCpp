26d5f24ac192960c71be7d27685352e161892f23
zhanglin2022 <1243265429@qq.com>
2026-04-27 22:49:11 +0800

    modify ex15_30

---
+++ Cpp-Primer/ch15/ex15.30/quote.cpp
@@ -0,0 +1,15 @@
+#include "quote.h"
+
+void Quote::debug() const {
+    std::cout << "data members of this class:\n"
+              << "BookNo: " << this->isbn() << " "
+              << "price: " << this->price << " ";
+}
+
+double print_total(std::ostream &os, const Quote &item, std::size_t n) {
+    double ret = item.net_price(n);
+    std::cout << "ISBN: " << item.isbn()
+              << " # sold: " << n
+              << " total due: " << ret << std::endl;
+    return ret;
+}
