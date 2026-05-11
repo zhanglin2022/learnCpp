c907c4844c77c169724ac25c629307bc812c3d0a
zhanglin2022 <1243265429@qq.com>
2026-05-11 23:28:39 +0800

    modify class BinaryQuery

--- Cpp-Primer/ch15/ex15.35.36.38/binaryquery.h
+++ Cpp-Primer/ch15/ex15.35.36.38/binaryquery.h
@@ -4,17 +4,26 @@
 #include "query_base.h"
 #include "query.h"
 
+/**
+ * @brief The BinaryQuery class
+ *An abstract class holds data needed by the query types that operate on two operands
+ */
 class BinaryQuery: public Query_base {
-// all private    
     BinaryQuery(const Query &l, const Query &r, std::string s) :
-        lhs(l), rhs(r), opSym(s) { }
-    std::string rep() const override { return "(" + lhs.rep() + 
-                                + " " + opSym + " " 
-                                + rhs.rep() + ")";
+        lhs(l), rhs(r), opSym(s) { 
+        std::cout << "BinaryQuery::BinaryQuery()  where s=" + s + "\n";            
     }
+
+    // @note:  abstract class: BinaryQuery doesn't define eval
+    std::string rep() const override { 
+        std::cout << "BinaryQuery::rep()\n";
+        return "(" + lhs.rep() + " " 
+                   + opSym + " " 
+                + rhs.rep() + ")";
+    }
+
     Query lhs, rhs;
     std::string opSym;
-
 };
 
 #endif
