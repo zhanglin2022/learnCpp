1a2695f583c5174eaa2c1fc604dee132b7e9c284
zhanglin2022 <1243265429@qq.com>
2026-05-15 23:07:29 +0800

    add function eval

---
+++ Cpp-Primer/ch15/ex15.39.40/andquery.cpp
@@ -0,0 +1,11 @@
+#include "andquery.h"
+#include "algorithm"
+
+QueryResult AndQuery::eval(const TextQuery &text) const {
+    auto left = lhs.eval(text), right = rhs.eval(text);
+    auto ret_lines = std::shared_ptr<std::set<line_no>>();
+    std::set_intersection(left.begin(), left.end(), 
+                          right.begin(), right.end(), 
+                          std::inserter(*ret_lines, ret_lines->begin()));
+    return QueryResult(rep(), ret_lines, left.get_file());
+}
