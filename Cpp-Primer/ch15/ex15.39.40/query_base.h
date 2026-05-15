72d686e286951c89eb205070b17a2b6c7e806b06
zhanglin2022 <1243265429@qq.com>
2026-05-16 00:17:51 +0800

    complete ex15_39

---
+++ Cpp-Primer/ch15/ex15.39.40/query_base.h
@@ -0,0 +1,23 @@
+#ifndef QUERY_BASE_H
+#define QUERY_BASE_H
+
+#include "textquery.h"
+#include "queryresult.h"
+
+/**
+ * abstract class acts as a basic class for all concrete query types.
+ * all members are private.
+ */
+class Query_base {
+    friend class Query;
+protected:
+    using line_no = TextQuery::line_no;  // used in the eval function
+    virtual ~Query_base() = default;
+private:
+    // return QueryResult that mathes this query
+    virtual QueryResult eval(const TextQuery&) const =0;
+    // a string representation of this query
+    virtual std::string rep() const = 0;
+};
+
+#endif // QUERY_BASE_H
---
+++ Cpp-Primer/ch15/ex15.39.40/queryresult.cpp
@@ -0,0 +1,26 @@
+//
+// Exercise 12.33:
+// In Chapter 15 we’ll extend our query system and will need some additional
+// members in the QueryResult class.
+//
+// Add members named begin and end that
+// return iterators into the set of line numbers returned by a given query,
+// and a member named get_file that returns a shared_ptr to the file in the
+// QueryResult object.
+//
+
+#include "queryresult.h"
+
+/**
+ * @brief   print the result to the output stream specified.
+ * @note    class QueryResult's friend
+ */
+std::ostream& operator<<(std::ostream &os, const QueryResult &qr) {
+    os << qr.sought << " occurs " << qr.lines->size() << (qr.lines->size() > 1 ? " times" : " time") << std::endl;
+    
+    for (auto index : *qr.lines) {
+        ConstStrBlobPtr wp(qr.file, index);
+        os << "\t(line " << index + 1 << ") " << wp.deref() << std::endl;
+    }
+    return os;
+}
