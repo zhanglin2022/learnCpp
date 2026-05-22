81f8c1ff02ea5f24c970e03c257f1469241eb5af
zhanglin2022 <1243265429@qq.com>
2026-05-22 11:40:13 +0800

    add ex16_5

---
+++ Cpp-Primer/ch16/ex16.5/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+template<typename Arr>
+void print(Arr const &arr) {
+    for (auto ele : arr)
+        std::cout << ele << std::endl;
+}
+
+int main() {
+    std::string sa[] = {"C++", "Primer", "5th"};
+    int ia[] = {2, 4, 6};
+    print(sa);
+    print(ia);
+    
+    return 0;
+}
