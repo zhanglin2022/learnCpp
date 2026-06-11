95278e104e6ab8715b0777f09320730e6e345c0f
zhanglin2022 <1243265429@qq.com>
2026-06-11 23:50:53 +0800

    update class UniquePointer

--- Cpp-Primer/ch16/ex16.28/unique_pointer.h
+++ Cpp-Primer/ch16/ex16.28/unique_pointer.h
@@ -1,4 +1,34 @@
-#ifndef UNIQUE_PTR_H
-#define UNIQUE_PTR_H
+#ifndef UNIQUEPOINTER_H
+#define UNIQUEPOINTER_H
 
-#endif
+#include "delete.h"
+
+namespace CP5 {
+    // forward declarations for friendship
+    template<typename T, typename D>
+    class UniquePointer;
+
+    template<typename T, typename D>
+    void swap(UniquePointer<T, D>&, UniquePointer<T, D>&);
+
+    /**
+     * @brief std::unique_ptr like class template
+     */
+    template<typename T, typename D = CP5::Delete>
+    class UniquePointer {
+        friend void swap<T, D>(UniquePointer<T, D>&, UniquePointer<T, D>&);
+
+    public:
+        // default constructor
+        UniquePointer():
+            ptr(nullptr), deleter(D()) { }
+        // constructor that take a raw pointer
+        // TODO
+        
+    private:
+        T *ptr;
+        D deleter;
+    };
+
+}
+#endif // UNIQUEPOINTER_H
